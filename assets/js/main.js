dbe6bac476e73e250dfd389d4086347220423296
Colleen Kusoski <colleen.kusoski@inserv-worldwide.com>
2015-05-27 11:25:41 -0400

    fixing cross domain issues, adding redirects, updating header menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,18 +3,53 @@ $(function(e) {
 
 	// plant invitation cookie to let folks know they can plant the paper
 	if (!$.cookie('plant_your_invites')) {
-	    $( "#plant-invites" ).show();
-	    $(".close").click(function() {
-	        $( "#plant-invites" ).slideUp( "slow" );
+		//$('body').css({'padding' : '285px 0 0 0'});
+	    $('#plant-invites').show();
+	    $('.close').click(function() {
+	        $('#plant-invites').slideUp( 'slow' );
 	        // set the cookie until you remove cookie
 	        $.cookie('plant_your_invites', true, { expires: 90 });
+	        if ($('#plant-invites').hasClass('active')) {
+	        	$('#plant-invites').removeClass('active');
+	        }
+	        //$('body').css({'padding' : '215px 0 0 0'});
 	    });
+	} else {
+		//$('body').css({'padding' : '215px 0 0 0'});
+	}
+
+	// Create a clone of the menu, right next to original.
+	$('.nav-container').addClass('original').clone().insertAfter('.nav-container').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
+
+	scrollIntervalID = setInterval(stickIt, 10);
+
+
+	function stickIt() {
+
+	  var orgElementPos = $('.original').offset();
+	  orgElementTop = orgElementPos.top;               
+
+	  if ($(window).scrollTop() >= (orgElementTop)) {
+	    // scrolled past the original position; now only show the cloned, sticky element.
+
+	    // Cloned element should always have same left position and width as original element.     
+	    orgElement = $('.original');
+	    coordsOrgElement = orgElement.offset();
+	    leftOrgElement = coordsOrgElement.left;  
+	    widthOrgElement = orgElement.css('width');
+	    $('.cloned').css('left',leftOrgElement+'px').css('top',0).css('width',widthOrgElement).show();
+	    $('.original').css('visibility','hidden');
+	  } else {
+	    // not scrolled past the menu; only show the original menu.
+	    $('.cloned').hide();
+	    $('.original').css('visibility','visible');
+	  }
 	}
 	
 
-	var header = $('header').outerHeight() + 20;
+	var header = $('.nav-container').outerHeight() + 15;
 
-	$("nav ul a, #thanks a").click(function(e) {
+	$('nav ul a.single-page, #thanks a').click(function(e) {
 	    e.preventDefault();
 	    var id = $(this).attr('href').substring(1);
 	    $('html,body').animate({scrollTop: $('#'+id).offset().top - header}, 'fast');
@@ -24,10 +59,10 @@ $(function(e) {
 	//console.log((window.location.pathname.match(/^\//i)));
 
 	// scroll to rsvp section when url is input into address bar
-	if (document.location.pathname.indexOf("/rsvp") == 0) {
+	/*if (document.location.pathname.indexOf('/rsvp') == 0) {
     	$('html,body').animate({
     		scrollTop: $('#rsvp').offset().top - header}, 'fast'
     	);
-	}
+	}*/
 
 });
--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -140,3 +140,132 @@
     };
 
 }));
+
+/*global window */
+
+/* Demonstration of cross-domain security workaround
+   by Philip Hutchison, October 2010
+   MIT-style license. Use at your own risk.
+   This example's code can be made much more concise, but I'm using long form for clarity in the example
+   See explanation at http://pipwerks.com/2010/10/17/iframes-and-cross-domain-security-part-3/
+*/
+
+//Returns boolean indicating whether parent frame is accessible
+function isSameDomain(){
+    
+    //Using try/catch to check for top.location so we don't throw a JS error
+    try {
+        return !!top.location.href;
+    } catch (e){
+        return false;
+    }
+    
+}
+
+
+function useProxy(msg){
+    
+    //var 'msg' will be sent to proxy HTML file via a querystring.
+    //This function will prep the string, then send to proxy 
+
+    var querystring = window.location.href.split("?")[1] || false,
+        id = "proxyframe", //The name of the frame
+        proxy = frames[id], //Look for existing frame with name "proxyframe"
+        url_array,
+        root = "",
+        pairs,
+        url;
+
+    if(!querystring || !msg){ return false; }
+    
+
+    // --- Prep/clean up string ---
+    
+    //Encode any question marks since they're reserved for querystrings
+    msg = msg.replace(/\?/gi, "%3F");
+    
+    //URI encode the entire string
+    msg = encodeURI(msg);
+    
+    
+    // --- Send to proxy via querystring ---
+
+    //First we must determine what domain is hosting the proxy file
+    //by examining the querystring for our custom 'root' parameter
+        
+    //Ensure all ampersands are single (not entities) so we can split using "&"
+    querystring = querystring.replace(/&amp;/g, "&");    
+    pairs = querystring.split("&");
+
+    //Loop through the pairs.
+    for(var i=0; i < pairs.length; i++){
+
+        if(pairs[i].indexOf("root=") !== -1){
+
+            //Extract the value from the string by replacing the
+            //identifier/assignment portion of the string with nothing ""
+            root = pairs[i].replace("root=", "");    
+            
+            if(root !== ""){
+                
+                //a root parameter was found. decode it.
+                root = decodeURI(root);
+                
+                //replace parent page filename in root parameter
+                //with proxy filename and querystring
+                url_array = root.split("/");                
+                url_array[url_array.length -1] = "proxy.html?msg=" +msg;
+                
+                //Set new URL
+                url = url_array.join("/");                
+            
+            }
+            
+            break; //We only care about the 'root' parameter
+            
+        }
+
+    }
+    
+    //If the proxy iframe has already been created, use it
+    if(proxy){
+
+        //Redirect to the new URL
+        proxy.location.href = url;
+
+    //else create the proxy iframe
+    } else {
+
+        var iframe = document.createElement("iframe");
+        iframe.id = id;
+        iframe.name = id;
+        iframe.src = url;
+        iframe.style.display = "none";
+        document.body.appendChild(iframe);
+
+    }
+    
+}
+
+
+function sendMessage(){
+
+    var msg = document.getElementById("message").value;
+
+    //If domains match, no need for proxy workaround.
+    if(parent && isSameDomain()){
+    
+        //Invoke parent's JavaScript directly
+        top.showMessage(msg);
+    
+    } else {
+        
+        //Security restrictions prevent us from communicating
+        //directly with parent. Must use proxy instead.
+        useProxy(msg);
+        
+    }
+
+    return false;
+
+}
